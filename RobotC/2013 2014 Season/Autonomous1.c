454e5d92b8447aadd5c633328938865af9c6c7b2
Cobra5 <casa.akaash@gmail.com>
2013-12-01 12:42:17 -0600

    Started Autonomous
    Added Motors, and got simple code to go to the ramp

--- RobotC/2013 2014 Season/Autonomous1.c
+++ RobotC/2013 2014 Season/Autonomous1.c
@@ -1,9 +1,9 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  none,     none)
 #pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Motor,  mtr_S1_C1_1,     motor2,        tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S1_C1_2,     motor3,        tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S1_C2_1,     motor1,        tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_2,     motor4,        tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C1_1,     armMotor,      tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C1_2,     rightMotor,    tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C2_1,     scoopMotor,    tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C2_2,     leftMotor,     tmotorTetrix, openLoop)
 
 #include "JoystickDriver.c"
 
@@ -35,10 +35,23 @@ task main()
 
 	waitForStart();
 
-	motor[motor1] = 100;
-	motor[motor3] = -100;
-	wait1Msec(1000);
-	motor[motor1] = 0;
-	motor[motor3] = 0;
+	motor[rightMotor] = 100;
+	motor[leftMotor] = 100;
+	wait1Msec(2250);
+	motor[rightMotor] = 0;
+	motor[leftMotor] = 0;
+
+	motor[leftMotor] = 100;
+	motor[rightMotor] = 0;
+	wait1Msec(2000);
+	motor[leftMotor] = 0;
+	motor[rightMotor] = 0;
+
+	motor[rightMotor] = 100;
+	motor[leftMotor] = 100;
+	wait1Msec(2250);
+	motor[rightMotor] = 0;
+	motor[leftMotor] = 0;
+
 
 }
